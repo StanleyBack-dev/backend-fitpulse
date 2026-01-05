aaf878840f590cac850557841b228fff39e57b1e
Stanley Barros Rodrigues <dev03.dlp@dolpengenharia.local>
2026-01-05 16:45:00 -0300

    fix: test news configs vercel deploy

--- serverless.ts
+++ serverless.ts
@@ -1,25 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module';
 import { ConfigService } from '@nestjs/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
-import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { createServer } from 'http';
 
+const expressApp = express();
 let server: ReturnType<typeof createServer>;
 
-async function handler(req: IncomingMessage, res: ServerResponse) {
-  if (!server) {
-    const app = await NestFactory.create(AppModule);
-    app.use(cookieParser());
+async function handler(req: Request, res: Response) {
+  try {
+    if (!server) {
+      const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+      app.use(cookieParser());
 
-    const config = app.get(ConfigService);
-    const corsOptions = config.get('cors');
-    app.enableCors(corsOptions);
+      const config = app.get(ConfigService);
+      const corsOptions = config.get('cors');
+      app.enableCors(corsOptions);
 
-    await app.init();
-    server = createServer(app.getHttpAdapter().getInstance());
+      await app.init();
+      server = createServer(expressApp);
+    }
+
+    return server.emit('request', req, res);
+  } catch (err) {
+    console.error('Erro ao inicializar NestJS:', err);
+    res.statusCode = 500;
+    res.end('Erro interno ao iniciar o servidor');
   }
-
-  return server.emit('request', req, res);
 }
 
 module.exports = handler;
