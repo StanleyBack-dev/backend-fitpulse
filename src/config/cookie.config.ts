c0cbe3237948100bc4bcc93c6dfe40a1a3d3880f
Stanley Barros Rodrigues <dev03.dlp@dolpengenharia.local>
2026-01-06 13:36:43 -0300

    fix: update logic config cookies

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -7,7 +7,7 @@ export const REFRESH_TOKEN_COOKIE_NAME = 'refreshToken';
 export const REFRESH_TOKEN_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: IS_PRODUCTION,
-  sameSite: IS_PRODUCTION ? 'none' : 'lax',
+  sameSite: IS_PRODUCTION ? 'none' : 'none',
   path: '/',
   maxAge: 30 * 24 * 60 * 60 * 1000,
 };
